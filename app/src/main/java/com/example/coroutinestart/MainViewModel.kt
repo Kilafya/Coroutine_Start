e74c2453c7497e428af5d847d93fdf18543bdfcd
drayl <sunrise.161@mail.ru>
2022-07-01 16:00:41 +0300

    cancel job

--- app/src/main/java/com/example/coroutinestart/MainViewModel.kt
+++ app/src/main/java/com/example/coroutinestart/MainViewModel.kt
@@ -1,43 +1,40 @@
 package com.example.coroutinestart
 
 import android.util.Log
+import android.util.LogPrinter
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.*
 import java.lang.RuntimeException
 
 
 class MainViewModel: ViewModel() {
 
-    private val parentJob = Job()
-    private val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
-        Log.d(LOG_TAG, "Exception: $throwable")
-    }
-    private val coroutineScope = CoroutineScope(Dispatchers.Main + parentJob + exceptionHandler)
-
     fun method() {
-        val childJob1 = coroutineScope.launch {
+        val job = viewModelScope.launch(Dispatchers.Default) {
+            Log.d(LOG_TAG, "Coroutine started")
+            val before = System.currentTimeMillis()
+            var count = 0L
+            for (i in 0 until 100_000_000) {
+                for (j in 0 until 100) {
+//                    if (isActive) {
+//                        ++count
+//                    } else {
+//                        throw CancellationException()
+//                    }
+                    ensureActive()
+                    ++count
+                }
+            }
+            Log.d(LOG_TAG, "Work time ${System.currentTimeMillis() - before}")
+        }
+        job.invokeOnCompletion {
+            Log.d(LOG_TAG, "Coroutine finished | $it")
+        }
+        viewModelScope.launch {
             delay(3000)
-            Log.d(LOG_TAG, "first coroutine finished")
+            job.cancel()
         }
-        val childJob2 = coroutineScope.launch {
-            delay(2000)
-            Log.d(LOG_TAG, "second coroutine finished")
-        }
-        val childJob3 = coroutineScope.async {
-            delay(1000)
-            error()
-            Log.d(LOG_TAG, "third coroutine finished")
-        }
-
-    }
-
-    private fun error() {
-        throw RuntimeException()
-    }
-
-    override fun onCleared() {
-        super.onCleared()
-        coroutineScope.cancel()
     }
 
     companion object {
